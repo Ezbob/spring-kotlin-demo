104af0a97ad96d337ef50fc2197160029d90cda7
Anders Busch <andersbusch@gmail.com>
2019-09-04 19:51:13 +0200

    initial commit

---
+++ src/main/kotlin/com/example/demo/BlogConfiguration.kt
@@ -0,0 +1,31 @@
+package com.example.demo
+
+import org.springframework.boot.ApplicationRunner
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+
+@Configuration
+class BlogConfiguration {
+
+    @Bean
+    fun databaseInitializer(userRepository: UserRepository,
+                            articleRepository: ArticleRepository) = ApplicationRunner {
+        val smaldini = userRepository.save(User(
+                login = "smaldini",
+                firstname = "Stephane",
+                lastname = "Maldini"
+        ))
+        articleRepository.save(Article(
+                title = "Reactor Bismuth is out",
+                headline = "Lorem ipsum",
+                content = "dolor set ament",
+                author = smaldini
+        ))
+        articleRepository.save(Article(
+                title = "Reactor Aluminium has landed",
+                headline = "Lorem ipsum",
+                content = "dolor set ament",
+                author = smaldini
+        ))
+    }
+}
